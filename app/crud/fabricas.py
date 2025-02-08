620ea9cdb9ca7ddd285f51e17543e67afdd653ce
Hiram Cornejo <131792188+soloyoloproXHD@users.noreply.github.com>
2025-02-07 22:29:23 -0600

    "Documentacion"

--- app/crud/fabricas.py
+++ app/crud/fabricas.py
@@ -2,24 +2,24 @@ from .models import Usuario, Zapato
 from abc import ABC, abstractmethod
 
 #CREATORS
-class Usuario_Creator(ABC):
+class Usuario_Creator(ABC): #Clase abstracta
     @abstractmethod
     def create(self, email: str, password: str):
         pass
 
-class Zapato_Creator(ABC):
+class Zapato_Creator(ABC): #Clase abstracta
     @abstractmethod
-    def create(self, marca: str, modelo: str, talla: int, color: str, precio: float, stock: int, descripcion: str, estado: bool):
+    def create(self, marca: str, modelo: str, talla: int, color: str, precio: float, stock: int, descripcion: str, estado: bool): 
         pass
 
 #CONCRETE CREATORS
-class Usuario_Factory(Usuario_Creator):
+class Usuario_Factory(Usuario_Creator): #Clase concreta
     def create(self, email: str, password: str) -> Usuario:
-        return Usuario.objects.create(email=email, password=password)
+        return Usuario.objects.create(email=email, password=password) #Se crea el objeto Usuario
 
-class Zapato_Factory(Zapato_Creator):
+class Zapato_Factory(Zapato_Creator): #Clase concreta
     def create(self, marca: str, modelo: str, talla: int, color: str, precio: float, stock: int, descripcion: str, estado: bool = True) -> Zapato:
-        return Zapato.objects.create(
+        return Zapato.objects.create( #Se crea el objeto Zapato
             marca=marca,
             modelo=modelo,
             talla=talla,
